34dd545a95d01d426ccaa6ed602d1adffda146a7
scottmotte <scott@scottmotte.com>
2010-04-06 20:27:11 -0700

    basic design coming together

---
+++ public/javascripts/index.js
@@ -0,0 +1,29 @@
+// var index_app = $.sammy(function() { with(this) {    
+//   // #/profile
+//   get('#/home', function() { with(this) {
+//     $(".static_videos .video").hide();
+//     $(".static_videos #intro").show();
+//     $(".static_videos .vid_link").removeClass('active');
+//     $(".static_videos #intro_link").addClass('active');
+//   }});
+//   
+//   // #/get_started
+//   get('#/get_started', function() { with(this) {
+//     $(".static_videos .video").hide();
+//     $(".static_videos #get_started").show();
+//     $(".static_videos .vid_link").removeClass('active');
+//     $(".static_videos #get_started_link").addClass('active');
+//   }});
+//   
+//   // #/paypal_setup
+//   get('#/paypal_setup', function() { with(this) {
+//     $(".static_videos .video").hide();
+//     $(".static_videos #paypal_setup").show();
+//     $(".static_videos .vid_link").removeClass('active');
+//     $(".static_videos #paypal_setup_link").addClass('active');
+//   }});
+// }});
+// 
+// $(function() {
+//   index_app.run();
+// });
