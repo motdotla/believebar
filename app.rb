00600192e31a33a08fd198aac7422e5c7f22abac
scottmotte <scott@scottmotte.com>
2010-04-10 15:09:32 -0700

    renames to app and uses sinatra application. this is getting rad. runs with unicorn

--- app.rb
+++ app.rb
@@ -1,16 +1,10 @@
 #!/usr/bin/env ruby
 require 'rubygems'
-require 'sinatra/base'
+require 'sinatra'
 require 'haml'
 
-class App < Sinatra::Base
+class App < Sinatra::Application
   get '/' do
     haml :index
-  end
-
-  # SASS stylesheet
-  get '/stylesheets/style.css' do
-    header 'Content-Type' => 'text/css; charset=utf-8'
-    sass :style
   end
 end
